db6cc649506227dcd947f701f6a77a6eade8ec60
Sergio Baquero <>
2019-03-30 17:37:06 -0500

    created entities and interfaces.

---
+++ src/main/java/co/com/sbaqueroadev/cyxtera/model/implementation/operation/AppOperation.java
@@ -0,0 +1,25 @@
+package co.com.sbaqueroadev.cyxtera.model.implementation.operation;
+
+
+import co.com.sbaqueroadev.cyxtera.exceptions.OperandsException;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+
+@Document
+public abstract class AppOperation{
+
+    @Id
+    private String id;
+
+    public AppOperation() {
+    }
+
+    public abstract Integer calculate(List<Integer> numbers) throws OperandsException;
+
+}
